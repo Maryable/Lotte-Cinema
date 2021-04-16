7d03fccf6e9572a49260b1307a3a4b0dfd43911c
Goeun Lee <rhdms608@gmail.com>
2021-04-16 21:53:45 +0900

    tab menu end

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
-const imgTop= {
-    "slideImg": ["slide0.jpg","slide1.jpg","slide2.jpg","slide3.jpg"]
-}
-$(function(){
-    $(".slide-cir p").on("click", function(){
-        no=$(this).index();
-        $(".slide-wrap").attr("src", "./img/slide"+no.imgTop.slideImg[no])
-    });
+'use strict'
 
-}); //jQuery end
+$(function(){
+    // 메인 슬라이드 
+  let mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+  });
+
+// 영화 차트 탭메뉴
+  let movie_btn = $('.movie_title>ul>li');
+  let movie_cont = $('.movie_chart > div');
+  movie_cont.hide().eq(0).show();
+  
+  movie_btn.click(function(e){
+    e.preventDefault();
+    let target = $(this);
+    let idx = target.index();
+    // alert(idx)
+    movie_btn.removeClass('active');
+    $(this).addClass('acitve');
+    movie_cont.css('display','none');
+    movie_cont.eq(idx).css('display','block');
+  });
+
+  let swiper = new Swiper('.swiper-container2', {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+});
